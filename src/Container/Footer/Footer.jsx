1771b8ecafc74fee8bfaa80add6ffebb2de8e5d5
Sourabhmalame@gmail.com <sourabhmalame@gmal.com>
2024-01-21 15:51:21 +0530

    All Done With Build

--- src/Container/Footer/Footer.jsx
+++ src/Container/Footer/Footer.jsx
@@ -32,14 +32,14 @@ const Footer = () => {
                 </div>
                 <div className="gpt3__footer-links_div">
                     <h4>Get in touch</h4>
-                    <p>Crechterwoord K12 182 DK Alknjkcb</p>
-                    <p>085-132567</p>
-                    <p>info@payme.net</p>
+                    <p>Sourabh Malame</p>
+                    <p>+91 9529023073</p>
+                    <p>sourabhmalame@gmail.com</p>
                 </div>
             </div>
 
             <div className="gpt3__footer-copyright">
-                <p>@2021 GPT-3. All rights reserved.</p>
+                <p>@2024 GPT-3. All rights reserved.</p>
             </div>
         </div>
     );
